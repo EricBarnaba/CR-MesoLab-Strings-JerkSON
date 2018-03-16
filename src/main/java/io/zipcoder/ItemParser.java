2b163c9b0dc2b0854326b0381bcb42a9877ba046
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-16 19:43:15 -0400

    making serious progress

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,8 +2,22 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
+
+    private int exceptions;
+   // private Pattern namePattern = Pattern.compile("([Mm][Ii][Ll][Kk]) | ([Bb][Rr][Ee][Aa][Dd]) | ([Cc][Oo][Oo][Kk][Ii][Ee][Ss]) | ([Aa][Pp][Pp][Ll][Ee][Ss])");
+    private Pattern wordPattern = Pattern.compile("(:[a-zA-Z|\\d]+)[^./@;!%*^]");
+    private Pattern pricePattern = Pattern.compile("(\\d+\\.\\d{2})");
+    //private Pattern typePattern = Pattern.compile("([Ff][Oo][Oo][Dd])");
+    //private Pattern typePattern = Pattern.compile("\\w+");
+    private Pattern expirationPattern = Pattern.compile("(\\d+/\\d{2}/\\d{4})");
+
+    public ItemParser(){
+        exceptions = 0;
+    }
 
 
     public ArrayList<String> parseRawDataIntoStringArray(String rawData){
@@ -13,11 +27,33 @@ public class ItemParser {
     }
 
     public Item parseStringIntoItem(String rawItem) throws ItemParseException{
-        return null;
+        String name;
+        String type;
+        Matcher words = wordPattern.matcher(rawItem);
+        if(!words.find()) throw new ItemParseException();
+        name = words.group().substring(1);
+        name = name.toLowerCase();
+        name = name.substring(0,1).toUpperCase() + name.substring(1);
+        if(name.contains("0"))name =  name.replace('0','o');
+        if(!words.find()) throw new ItemParseException();
+        type = words.group().substring(1);
+
+
+        Double price ;
+        Matcher dollars = pricePattern.matcher(rawItem);
+        if(!dollars.find()) throw new ItemParseException();
+        price = Double.valueOf(dollars.group());
+
+        String expiration;
+        Matcher date = expirationPattern.matcher(rawItem);
+        if(!date.find()) throw new ItemParseException();
+        expiration = date.group();
+
+        return new Item(name,price,type,expiration);
     }
 
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
-        String stringPattern = "[;|^]";
+        String stringPattern = "[;^%@!*]";
         ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawItem);
         return response;
     }
--- src/test/java/io/zipcoder/ItemParserTest.java
+++ src/test/java/io/zipcoder/ItemParserTest.java
@@ -12,13 +12,17 @@ public class ItemParserTest {
 
     private String rawSingleItem =    "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##";
 
+    private String weirdSingleItem = "naMe:Co0KIes%price:3.23^type:Food*expiration:1/25/2016##";
+
+    private String weirdSingleItem2 = "naMe:Milk%price:3.23!type:Food@expiration:1/25/2016##";
+
     private String rawSingleItemIrregularSeperatorSample = "naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##";
 
-    private String rawBrokenSingleItem =    "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##";
+    private String rawBrokenSingleItem =    "naMe:Milk;price:;type:Food;expiration:1/25/2016##";
 
     private String rawMultipleItems = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##"
-                                      +"naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##"
-                                      +"NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##";
+                                      +"naME:BreaD%price:1.23^type:Food*expiration:1/02/2016##"
+                                      +"NAMe%BrEAD;price:1.23!type:Food@expiration:2/25/2016##";
     private ItemParser itemParser;
 
     @Before
@@ -36,8 +40,15 @@ public class ItemParserTest {
 
     @Test
     public void parseStringIntoItemTest() throws ItemParseException{
-        Item expected = new Item("milk", 3.23, "food","1/25/2016");
+        Item expected = new Item("Milk", 3.23, "Food","1/25/2016");
         Item actual = itemParser.parseStringIntoItem(rawSingleItem);
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    public void parseWeirdStringIntoItemTest() throws ItemParseException{
+        Item expected = new Item("Cookies", 3.23, "Food","1/25/2016");
+        Item actual = itemParser.parseStringIntoItem(weirdSingleItem);
         assertEquals(expected.toString(), actual.toString());
     }
 
@@ -50,6 +61,20 @@ public class ItemParserTest {
     public void findKeyValuePairsInRawItemDataTest(){
         Integer expected = 4;
         Integer actual = itemParser.findKeyValuePairsInRawItemData(rawSingleItem).size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findKeyValuePairsInWeirdItemDataTest(){
+        Integer expected = 4;
+        Integer actual = itemParser.findKeyValuePairsInRawItemData(weirdSingleItem).size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findKeyValuePairsInWeirdItemData2Test(){
+        Integer expected = 4;
+        Integer actual = itemParser.findKeyValuePairsInRawItemData(weirdSingleItem2).size();
         assertEquals(expected, actual);
     }
 

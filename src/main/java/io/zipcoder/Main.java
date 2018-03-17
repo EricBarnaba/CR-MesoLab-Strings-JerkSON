2b13013f9c1676039a418a303a83c326fa0c05f3
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-16 20:09:53 -0400

    saving for the night

--- src/main/java/io/zipcoder/Main.java
+++ src/main/java/io/zipcoder/Main.java
@@ -2,13 +2,33 @@ package io.zipcoder;
 
 import org.apache.commons.io.IOUtils;
 
+import java.util.ArrayList;
+
 
 public class Main {
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
-        return result;
+        ItemParser jerkSon = new ItemParser();
+        StringBuilder builder = new StringBuilder();
+        ArrayList<String> text = jerkSon.parseRawDataIntoStringArray(result);
+        ArrayList<ArrayList<String>> keyValuePairs = new ArrayList<>();
+        for(String s : text){
+            keyValuePairs.add(jerkSon.findKeyValuePairsInRawItemData(s));
+        }
+        for(ArrayList<String> items : keyValuePairs){
+            for(String s : items){
+                try {
+                    Item temp = jerkSon.parseStringIntoItem(s);
+                    System.out.println(temp.toString());
+                }
+                catch(ItemParseException ipe){
+
+                }
+            }
+        }
+        return builder.toString();
     }
 
     public static void main(String[] args) throws Exception{
